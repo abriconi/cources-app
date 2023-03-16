81494a3983c72f3a0138df56674a939f7d8ddf35
Alyona Gepting <abriconi@gmail.com>
2023-03-16 17:01:37 +0200

    Add types to component Courses.tsx fixed errors

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,12 @@ export type Author = {
 	id: string;
 	name: string;
 };
+
+export type Course = {
+	id: string;
+	title: string;
+	description: string;
+	creationDate: string;
+	duration: number;
+	authors: string[];
+};
