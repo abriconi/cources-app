dd84c5d0c8724a8b3f31d9778ecc5f126a9aee66
Alyona Gepting <abriconi@gmail.com>
2023-03-16 17:01:37 +0200

    Add types to component Courses.tsx fixed errors

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SearchBar from './components/SearchBar/Seacrbar';
 import Button from '../common/Button/Button';
 import CourseCard from './components/CourseCard/CourseCard';
 import CreateCourse from '../CreateCourse/CreateCourse';
 import { mockedCoursesList, BUTTON_TEXT, mockedAuthorsList } from '../constans';
+import { Course, Author } from '../../interfaces/index';
 
 import './courses.css';
 import filterCourses from '../helpers/filterCourses';
 
 function Courses() {
-	const [courses, setCourses] = useState(mockedCoursesList);
-	const [authorsList, setAuthorsList] = useState(mockedAuthorsList);
+	const [courses, setCourses] = React.useState<Course[]>(mockedCoursesList);
+	const [authorsList, setAuthorsList] =
+		React.useState<Author[]>(mockedAuthorsList);
 
-	function searchCoursesHandle(searchText) {
-		const filteredCourses = filterCourses(mockedCoursesList, searchText);
+	function searchCoursesHandle(searchText: string): void {
+		const filteredCourses: Course[] = filterCourses(
+			mockedCoursesList,
+			searchText
+		);
 		setCourses(filteredCourses);
 	}
 
@@ -21,13 +26,13 @@ function Courses() {
 		<div className='coursesWrapper'>
 			<div className='searchBarWrapper'>
 				<SearchBar onSearch={searchCoursesHandle}></SearchBar>
-				<Button buttonText={BUTTON_TEXT.addNewCourse}></Button>
+				<Button buttonText={BUTTON_TEXT.addNewCourse} type={'button'}></Button>
 			</div>
 			{courses.map((course) => (
 				<CourseCard key={course.title} courseData={course}></CourseCard>
 			))}
 			<CreateCourse
-				authorsList={authorsList} // primary list of authors
+				authorsList={authorsList}
 				setAuthorsList={setAuthorsList}
 				courses={courses}
 				setCourses={setCourses}
