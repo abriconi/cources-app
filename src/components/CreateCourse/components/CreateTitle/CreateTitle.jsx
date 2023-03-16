fd6ecf28f2c00b82f2c2e6a0f30369d00b1466ef
Alyona Gepting <abriconi@gmail.com>
2023-03-16 16:43:06 +0200

    Add types to Textarea attributes, fixed errors

--- src/components/CreateCourse/components/CreateTitle/CreateTitle.jsx
+++ src/components/CreateCourse/components/CreateTitle/CreateTitle.jsx
@@ -1,14 +1,19 @@
 import Input from '../../../common/Input/Input';
 import Textarea from '../../../common/Textarea/Textrarea';
+import { PLACEHOLDER_TEXT } from '../../../constans';
 
 import './createTitle.css';
 function CreateTitle() {
 	return (
 		<div className='titleWrapper'>
-			<Input labelText='Title' name='courseTitle' placeholder='Enter title' />
+			<Input
+				labelText='Title'
+				name='courseTitle'
+				placeholder={PLACEHOLDER_TEXT.enterTitle}
+			/>
 			<Textarea
 				minlength='2'
-				labelText='Description'
+				labelText={PLACEHOLDER_TEXT.description}
 				name='courseDescription'
 			/>
 		</div>
