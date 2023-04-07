eb675bca53a03c828ec860b44fdd2f35e186b725
Alyona Gepting <abriconi@gmail.com>
2023-04-07 15:17:36 +0300

    CreateCourse displayed only for user with a role admin, added message for users with other roles

--- src/components/CreateCourse/CreateCourse.tsx
+++ src/components/CreateCourse/CreateCourse.tsx
@@ -6,16 +6,24 @@ import { RootState } from '../../store';
 import { useNavigate } from 'react-router-dom';
 import { CoursePayload } from '../../interfaces';
 import { createCourse } from '../../store/courses/actionCreators';
+import { useSelector } from 'react-redux';
+import { getUser } from '../../store/selectors';
+import './createCourse.css';
 
 const CreateCourse: React.FC = () => {
 	const dispatch: ThunkDispatch<RootState, null, any> = useDispatch();
 	const navigate = useNavigate();
+	const userRole: any = useSelector(getUser).role;
 	const handleCourseSubmit = (course: CoursePayload) => {
 		dispatch(createCourse(course));
 		navigate('/courses');
 	};
 
-	return <CourseForm handleCourseSubmit={handleCourseSubmit} />;
+	return userRole === 'admin' ? (
+		<CourseForm handleCourseSubmit={handleCourseSubmit} />
+	) : (
+		<p className='messageForUser'>You must be an admin to create a course.</p>
+	);
 };
 
 export default CreateCourse;
